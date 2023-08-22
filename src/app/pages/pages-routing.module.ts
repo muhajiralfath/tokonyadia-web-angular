8dfc6792d69beb8d7bd5f5c40f02c4b6b8b6a0cc
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-22 22:48:08 +0700

    [FEATURE] add & set up routing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {CustFormComponent} from "./customer/cust-form/cust-form.component";
+import {CustListComponent} from "./customer/cust-list/cust-list.component";
+import {ProductFormComponent} from "./product/product-form/product-form.component";
+import {ProductListComponent} from "./product/product-list/product-list.component";
 
 const routes: Routes = [
-  // {
-  //   path: "", component: HomeComponent,children:
-  // }
+  {
+    path: "", component: HomeComponent,children: [
+      {path: "customer-form", component: CustFormComponent},
+      {path: "customer-list", component: CustListComponent},
+      {path: "product-form", component: ProductFormComponent},
+      {path: "product-list", component: ProductListComponent}
+    ]
+  }
 ];
 
 @NgModule({
