f2108cb3831acdbda67b86f11c93ff36cf1b6046
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-22 22:28:01 +0700

    [FEATURE] add 404 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogiinComponent } from './auth/logiin/logiin.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { NotfounfComponent } from './shared/component/notfounf/notfounf.component';
+import { NotfoundComponent } from './shared/component/notfound/notfound.component';
+import { MaintenanceComponent } from './shared/component/maintenance/maintenance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogiinComponent,
-    RegisterComponent
+    RegisterComponent,
+    NotfounfComponent,
+    NotfoundComponent,
+    MaintenanceComponent
   ],
   imports: [
     BrowserModule,
