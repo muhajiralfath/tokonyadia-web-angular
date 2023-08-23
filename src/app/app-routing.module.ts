a053ce9edd8b541c8f218a8ba65e4035e611d5ca
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-23 21:41:47 +0700

    [FEATURE] add auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {RegisterComponent} from "./auth/register/register.component";
 import {NotfoundComponent} from "./shared/component/notfound/notfound.component";
 import {LoginComponent} from "./auth/login/login.component";
+import {authGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
   {path: "login" , component: LoginComponent},
   {path: "register", component: RegisterComponent},
   {path: "", redirectTo: "login", pathMatch: "full"},
-  {path: "pages", loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule)},
+  {path: "pages", loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule), canActivate: [authGuard]},
   {path: "**", component: NotfoundComponent},
 ];
 
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,7 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  return !!sessionStorage.getItem('token');
+};
