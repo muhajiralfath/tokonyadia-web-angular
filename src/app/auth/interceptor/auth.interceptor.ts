09b3b38d1683509d3797f48cfcbd33352073caa9
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-23 20:04:37 +0700

    [FEATURE] add auth interceptor

--- src/app/auth/interceptor/auth.interceptor.ts
+++ src/app/auth/interceptor/auth.interceptor.ts
@@ -3,16 +3,50 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
+import {Router} from "@angular/router";
+import Swal from 'sweetalert2';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = sessionStorage.getItem('token');
+
+    const authRequest = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    return next.handle(authRequest)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: 'Unauthorized required',
+              showConfirmButton: false,
+              timer: 1500
+            })
+            this.router.navigate(['']);
+          }else if (error.status === 404) {
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: '404',
+              showConfirmButton: false,
+              timer: 1500
+            })
+            this.router.navigate(['404']);
+          }
+          return throwError(error);
+        })
+      );
   }
 }
