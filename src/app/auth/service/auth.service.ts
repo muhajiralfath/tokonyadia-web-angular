e56c019d227c3318ffcbda40718ec90690cadcaa
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-23 20:00:00 +0700

    [FEATURE] add authService

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {UserRequest} from "../model/user-request.model";
+import {Observable} from "rxjs";
+import {UserResponseWrapper} from "../model/user-response.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {
+  }
 
-  // public login
+  public login(data: UserRequest): Observable<any> {
+    return this.http.post<UserResponseWrapper>('api/auth/login', data);
+  }
+
+  public register(data: any): Observable<any> {
+    return this.http.post<UserResponseWrapper>('api/auth/register-admin', data)
+  }
 }
