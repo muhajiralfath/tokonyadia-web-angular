6b53a7664e6f3d4bbe7bed480e522a436309ecbe
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-23 17:24:41 +0700

    [FEATURE] add login & register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LogiinComponent} from "./auth/logiin/logiin.component";
 import {RegisterComponent} from "./auth/register/register.component";
 import {NotfoundComponent} from "./shared/component/notfound/notfound.component";
+import {LoginComponent} from "./auth/login/login.component";
 
 const routes: Routes = [
-  {path: "login" , component: LogiinComponent},
+  {path: "login" , component: LoginComponent},
   {path: "register", component: RegisterComponent},
-  {path: "**", component: NotfoundComponent},
   {path: "", redirectTo: "login", pathMatch: "full"},
-  {path: "pages", loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule)}
+  {path: "pages", loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule)},
+  {path: "**", component: NotfoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LogiinComponent } from './auth/logiin/logiin.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { NotfounfComponent } from './shared/component/notfounf/notfounf.component';
 import { NotfoundComponent } from './shared/component/notfound/notfound.component';
 import { MaintenanceComponent } from './shared/component/maintenance/maintenance.component';
+import {LoginComponent} from "./auth/login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogiinComponent,
     RegisterComponent,
-    NotfounfComponent,
     NotfoundComponent,
-    MaintenanceComponent
+    MaintenanceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
