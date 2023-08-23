793bc0544c4101a5dc662835d1306761854c540b
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-23 21:57:52 +0700

    [FEATURE] add import httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from './auth/register/register.component';
 import { NotfoundComponent } from './shared/component/notfound/notfound.component';
 import { MaintenanceComponent } from './shared/component/maintenance/maintenance.component';
 import {LoginComponent} from "./auth/login/login.component";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthInterceptor} from "./auth/interceptor/auth.interceptor";
 import {ReactiveFormsModule} from "@angular/forms";
 
@@ -22,7 +22,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true
